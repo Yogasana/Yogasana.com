56d7fd4cc0fbd8b26d395a90f2a200e10e6c6e05
Yogasana <143020017+Yogasana@users.noreply.github.com>
2023-10-06 22:17:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,139 +1,154 @@
-// Selecting the sidebar and buttons
-const sidebar = document.querySelector(".sidebar");
-const sidebarOpenBtn = document.querySelector("#sidebar-open");
-const sidebarCloseBtn = document.querySelector("#sidebar-close");
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
 
-// Function to hide the sidebar when the mouse leaves
-const hideSidebar = () => {
-    sidebar.classList.add("close");
-};
-
-// Function to show the sidebar when the mouse enters
-const showSidebar = () => {
-    sidebar.classList.remove("close");
-};
-
-// Function to show and hide the sidebar
-const toggleSidebar = () => {
-    sidebar.classList.toggle("close");
-};
-
-// If the window width is less than 800px, close the sidebar
-if (window.innerWidth < 800) {
-    sidebar.classList.add("close");
-}
-
-// Adding event listeners to buttons and sidebar for the corresponding actions
-sidebar.addEventListener("mouseleave", hideSidebar);
-sidebar.addEventListener("mouseenter", showSidebar);
-sidebarOpenBtn.addEventListener("click", toggleSidebar);
-sidebarCloseBtn.addEventListener("click", toggleSidebar);
-
-// ... (existing JavaScript code) ...
-
-// Your existing JavaScript code
-// ...
-
-// Add event listener to toggle night mode popup
-function toggleNightModePopup() {
-    const nightModePopup = document.getElementById('nightModePopup');
-    nightModePopup.classList.toggle('active');
-}
-
-// Event listener for night mode switch
-const switchBtn = document.querySelector('.radio-btn');
-const navBar = document.querySelector('nav');
-const card = document.querySelector('.nc');
-const card1 = document.querySelector('.nc1');
-const card2 = document.querySelector('.nc2');
-const card3 = document.querySelector('.nc3');
-const card4 = document.querySelector('.nc4');
-switchBtn.addEventListener('click', () => {
-    const radioInner = document.querySelector('.radio-inner');
-    document.body.classList.toggle('dark-mode');
-    navBar.classList.toggle('dark-mode'); // Toggle the dark mode class for the navigation bar
-    card.classList.toggle('dark-mode-card');
-      card1.classList.toggle('dark-mode-card');
-      card2.classList.toggle('dark-mode-card');
-      card3.classList.toggle('dark-mode-card');
-      card4.classList.toggle('dark-mode-card');
-    radioInner.classList.toggle('active');
+    menuToggle.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+    });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const arrowIcons = document.querySelectorAll(".bx-chevron-down");
 
+    arrowIcons.forEach(function (icon) {
+        icon.addEventListener("click", function () {
+            const answer = this.parentElement.querySelector(".answer");
 
-// Update user information function
-function updateUserInfo(user) {
-    document.getElementById('username').textContent = user.username;
-    document.getElementById('email').textContent = user.email;
-}
-
-// Retrieve the logged-in user information from local storage
-const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-
-if (loggedInUser) {
-    updateUserInfo(loggedInUser);
-} else {
-    window.location.href = './';
-}
-
-// Delete account function
-function deleteAccount() {
-    const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-
-    if (loggedInUser) {
-        localStorage.removeItem(loggedInUser.username);
-        localStorage.removeItem('loggedInUser');
-        alert('Account deleted successfully. You will be redirected to the registration page.');
-        window.location.href = './';
-    }
-}
-
-// Sign out function
-function signOut() {
-    localStorage.removeItem('loggedInUser');
-    window.location.href = './';
-}
-// Simulate content loading
-window.addEventListener('load', () => {
-  // Content loaded, gradually hide preloader
-  setTimeout(() => {
-    const preloader = document.querySelector('.preloader');
-    preloader.style.transition = 'opacity 2s ease, z-index 2s ease';
-    preloader.style.opacity = '0';
-    preloader.style.zIndex = '-1';
-  }, 3000); // Add a delay of 2000ms (2 seconds) before hiding the preloader
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    });
 });
-let cards = document.querySelectorAll(".card");
-     let stackArea = document.querySelector(".stack-area");
+document.addEventListener("DOMContentLoaded", function () {
+    const arrowIcons = document.querySelectorAll(".bx-chevron-down");
 
-     function rotateCards() {
-       let angle = 0;
-       cards.forEach((card) => {
-         if (card.classList.contains("active")) {
-           card.style.transform = `translate(-50%, -120vh) rotate(-48deg)`;
-         } else {
-           card.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
-           angle = angle - 10;
-         }
-       });
-     }
+    arrowIcons.forEach(function (icon) {
+        icon.addEventListener("click", function () {
+            const answer = this.parentElement.nextElementSibling;
 
-     rotateCards();
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+                this.parentElement.parentElement.style.height = "40px"; // Adjust the width when collapsing
+            } else {
+                answer.style.display = "block";
+                this.parentElement.parentElement.style.height = "120px"; // Adjust the width when expanding
+            }
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const hindiButton = document.getElementById("hindi-button");
+    const englishButton = document.getElementById("english-button");
+    const videoSource = document.getElementById("video-source");
 
-     window.addEventListener("scroll", () => {
-       let proportion =
-         stackArea.getBoundingClientRect().top / window.innerHeight;
-       if (proportion <= 0) {
-         let n = cards.length;
-         let index = Math.ceil((proportion * n) / 2);
-         index = Math.abs(index) - 1;
-         for (let i = 0; i < n; i++) {
-           if (i <= index) {
-             cards[i].classList.add("active");
-           } else {
-             cards[i].classList.remove("active");
-           }
-         }
-         rotateCards();
-       }
-     });
+    hindiButton.addEventListener("click", function () {
+        // Change the video source to the Hindi version
+        videoSource.src = "Improve Your Digestion With Padmastask - Sanyukt Asana.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video").load();
+    });
+
+    englishButton.addEventListener("click", function () {
+        // Change the video source to the English version
+        videoSource.src = "Indigestion.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video").load();
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const hindiButton = document.getElementById("hindi-button-s");
+    const englishButton = document.getElementById("english-button-s");
+    const videoSource = document.getElementById("custom-video-s");
+
+    hindiButton.addEventListener("click", function () {
+        // Change the video source to the Hindi version
+        videoSource.src = "eye.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-s").load();
+    });
+
+    englishButton.addEventListener("click", function () {
+        // Change the video source to the English version
+        videoSource.src = "eyeEng.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-s").load();
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const hindiButton = document.getElementById("hindi-button-w");
+    const englishButton = document.getElementById("english-button-w");
+    const videoSource = document.getElementById("custom-video-w");
+
+    hindiButton.addEventListener("click", function () {
+        // Change the video source to the Hindi version
+        videoSource.src = "b1.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-w").load();
+    });
+
+    englishButton.addEventListener("click", function () {
+        // Change the video source to the English version
+        videoSource.src = "b1eng.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-w").load();
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const hindiButton = document.getElementById("hindi-button-sh");
+    const englishButton = document.getElementById("english-button-sh");
+    const videoSource = document.getElementById("custom-video-sh");
+
+    hindiButton.addEventListener("click", function () {
+        // Change the video source to the Hindi version
+        videoSource.src = "sh.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-sh").load();
+    });
+
+    englishButton.addEventListener("click", function () {
+        // Change the video source to the English version
+        videoSource.src = "sheng.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-sh").load();
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    const hindiButton = document.getElementById("hindi-button-h");
+    const englishButton = document.getElementById("english-button-h");
+    const videoSource = document.getElementById("custom-video-h");
+
+    hindiButton.addEventListener("click", function () {
+        // Change the video source to the Hindi version
+        videoSource.src = "hal.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-h").load();
+    });
+
+    englishButton.addEventListener("click", function () {
+        // Change the video source to the English version
+        videoSource.src = "haleng.mp4";
+        // Reload the video player with the new source
+        document.getElementById("custom-video-h").load();
+    });
+});
+// Get the night mode toggle button
+const nightModeToggle = document.getElementById('night-mode-toggle');
+
+// Listen for a click event on the button
+nightModeToggle.addEventListener('click', () => {
+  // Toggle the 'night-mode' class on the body element
+  document.body.classList.toggle('night-mode');
+  document.getElementById("cc");
+  // Update the button text and class
+  if (document.body.classList.contains('night-mode')) {
+    nightModeToggle.innerHTML = '<i class="bx bx-sun"></i> Day Mode';
+    nightModeToggle.classList.remove('night-mode-off');
+    nightModeToggle.classList.add('night-mode-on');
+  } else {
+    nightModeToggle.innerHTML = '<i class="bx bx-moon"></i> Night Mode';
+    nightModeToggle.classList.remove('night-mode-on');
+    nightModeToggle.classList.add('night-mode-off');
+  }
+});
